2011b5d8e9e4f5ba61a1346e54de363b9a30e760
Kurt-Mikhael <kurtmikhael123@gmail.com>
2026-05-21 23:45:23 +0700

    Feat: Action type

---
+++ src/main/java/banana/republic/player/ActionType.java
@@ -0,0 +1,15 @@
+package banana.republic.player;
+
+
+public enum ActionType {
+    BUILD_ROAD,
+    BUILD_SETTLEMENT,
+    BUILD_CITY,
+    BUY_DEV_CARD,
+    PLAY_KNIGHT,
+    PLAY_ROAD_BUILDING,
+    PLAY_MONOPOLY,
+    TRADE_DOMESTIC,
+    TRADE_MARITIME,
+    END_TURN
+}
