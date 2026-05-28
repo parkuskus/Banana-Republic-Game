f0485dd396923cde58570eaa3d97dcff8b065225
Vixrlie <vinctlie06@gmail.com>
2026-05-28 16:30:26 +0700

    feat: carddeck save data type

---
+++ src/main/java/banana/republic/save/CardDeckSaveData.java
@@ -0,0 +1,11 @@
+package banana.republic.save;
+
+import java.util.List;
+
+/**
+ * Card deck save data DTO.
+ */
+public class CardDeckSaveData {
+    public List<CardSaveData> drawPile;
+    public List<CardSaveData> discardPile;
+}
