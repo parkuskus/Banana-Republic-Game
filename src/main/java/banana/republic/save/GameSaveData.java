df5a804ab2aa8c9ff4c7dcf9aff73fb595ec1603
Vixrlie <vinctlie06@gmail.com>
2026-05-28 16:36:10 +0700

    refactor: attributesfor save

--- src/main/java/banana/republic/save/GameSaveData.java
+++ src/main/java/banana/republic/save/GameSaveData.java
@@ -1,9 +1,23 @@
 package banana.republic.save;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Data structure for saved game state.
  * Refer to class-diagram/Module5_UI_Plugin_Save.puml for full specification.
  */
 public class GameSaveData {
-    // TODO: Implement
+    public String version;
+    public String timestamp;
+    public String currentPhase;
+    public int turnNumber;
+    public int activePlayerIndex;
+    public int setupSettlementCount;
+    public TimerSaveData timer;
+    public Map<String, Integer> bank;
+    public BoardSaveData board;
+    public List<PlayerSaveData> players;
+    public CardDeckSaveData deck;
+    public DiceSaveData lastDice;
 }
