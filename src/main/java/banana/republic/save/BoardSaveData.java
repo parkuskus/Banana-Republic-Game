af787f55fd81fa8e3348a68c9b0e0f1a72fc71f1
Vixrlie <vinctlie06@gmail.com>
2026-05-28 16:33:33 +0700

    feat: board saveee

--- src/main/java/banana/republic/save/BoardSaveData.java
+++ src/main/java/banana/republic/save/BoardSaveData.java
@@ -1,9 +1,14 @@
 package banana.republic.save;
 
+import java.util.List;
+
 /**
  * Board save data DTO.
  * Refer to class-diagram/Module5_UI_Plugin_Save.puml for full specification.
  */
 public class BoardSaveData {
-    // TODO: Implement
+    public List<HexTileSaveData> hexTiles;
+    public List<IntersectionSaveData> intersections;
+    public List<PathSaveData> paths;
+    public List<HarborSaveData> harbors;
 }
