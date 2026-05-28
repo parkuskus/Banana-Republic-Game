3c6f7312f15bda27a3856836766921277a84e5b0
Vixrlie <vinctlie06@gmail.com>
2026-05-28 16:31:44 +0700

    refactor: harbor save data type

--- src/main/java/banana/republic/save/HarborSaveData.java
+++ src/main/java/banana/republic/save/HarborSaveData.java
@@ -1,9 +1,13 @@
 package banana.republic.save;
 
+import java.util.List;
+
 /**
  * Harbor save data DTO.
  * Refer to class-diagram/Module5_UI_Plugin_Save.puml for full specification.
  */
 public class HarborSaveData {
-    // TODO: Implement
+    public int id;
+    public String type;
+    public List<Integer> adjacentIntersectionIds;
 }
